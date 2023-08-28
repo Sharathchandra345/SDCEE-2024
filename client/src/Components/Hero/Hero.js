983e90fabab9ccd853bcb22f2b2017167866f209
Sharath Chandra <csharathchandra3@gmail.com>
2023-08-28 15:27:38 +0530

    Nav-Bar updated

--- client/src/Components/Hero/Hero.js
+++ client/src/Components/Hero/Hero.js
@@ -1,30 +1,35 @@
-import React from 'react'
-import "./Hero.css"
+import React from "react";
+import "./Hero.css";
 
 export default function Hero() {
   return (
     <>
-    <div className='hero'>
+      <div className="hero">
         <div className="hero-left">
           <div className="hero-text">
-            <h1 className='hero-head'> SDCEE '24 </h1>
-            <h5> International Conference on <br /> "Sustainable Development in Chemical and <br /> Environmental Engineering"</h5>
+            <h1 className="hero-head"> SDCEE '24 </h1>
+            <h5>
+              {" "}
+              International Conference on <br /> "Sustainable Development in
+              Chemical and <br /> Environmental Engineering"
+            </h5>
 
-            <h3 className='mt-5'> Feburary 22-24, 2024 </h3>
-            <button className='hero-reg mt-3'>Register</button>
+            <h3 className="mt-5"> Feburary 22-24, 2024 </h3>
+            <button className="hero-reg mt-3">Register</button>
           </div>
-          
-      </div>
-        <div className="hero-right">
-
         </div>
+        <div className="hero-right"></div>
       </div>
 
       <div className="wave">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-              <path fill="000000" fill-opacity="1" d="M0,224L34.3,208C68.6,192,137,160,206,170.7C274.3,181,343,235,411,256C480,277,549,267,617,234.7C685.7,203,754,149,823,122.7C891.4,96,960,96,1029,117.3C1097.1,139,1166,181,1234,208C1302.9,235,1371,245,1406,250.7L1440,256L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+          <path
+            fill="000000"
+            fill-opacity="1"
+            d="M0,96L80,128C160,160,320,224,480,213.3C640,203,800,117,960,90.7C1120,64,1280,96,1360,112L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+          ></path>
         </svg>
       </div>
     </>
-  )
+  );
 }
