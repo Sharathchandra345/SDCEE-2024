1e4b73d2adb48e443a815198ee5403fd5d18586b
Sharath Chandra Chinnarigari <csharathchandra3@gmail.com>
2023-09-03 14:21:14 +0530

    updates

--- client/src/Components/Hero/Hero.js
+++ client/src/Components/Hero/Hero.js
@@ -24,7 +24,7 @@ export default function Hero() {
       </div>
 
       <div className="wave">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 250">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 230">
           <path
             fill="#ffffff"
             fill-opacity="1"
